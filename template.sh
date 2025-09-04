c7443d4742f68369d79de2857b3699945f88e6fc
Hassaan Huzaifa <mindb2929@gmail.com>
2025-09-04 23:49:31 +0500

    Update template.sh

--- template.sh
+++ template.sh
@@ -8,3 +8,10 @@ touch src/helper.py
 touch src/prompt.py
 touch .env 
 
+touch setup.py
+touch app.py
+touch research/trials.ipynb 
+touch requirements.txt
+
+
+echo "Directory and files created successfully!."
