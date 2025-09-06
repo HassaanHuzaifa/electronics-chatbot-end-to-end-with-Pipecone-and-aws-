6c2d9b2455f5aa6e13001ff66268d8886ae7743a
Hassaan Huzaifa <mindb2929@gmail.com>
2025-09-06 23:01:54 +0500

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -34,3 +34,15 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
 
 
 
+#Split the Data into Text Chunks
+def text_split(extracted_data):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+
+
+
+#Download the Embeddings from HuggingFace 
+def download_hugging_face_embeddings():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
+    return embeddings
